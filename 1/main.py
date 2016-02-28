7f441c76455006feee12962c27c93e0c04553137
become-iron <nikitaburkov@yandex.ru>
2016-02-28 12:06:01 +0300

    добавлено вычисление по заданному количеству членов ряда

--- 1/main.py
+++ 1/main.py
@@ -6,24 +6,56 @@
 from math import factorial, fabs  # высчитывание факториала, модуля числа
 from decimal import Decimal  # для точных расчётов
 
-sum_of_series = 1  # сумма ряда, включая первый его член равный 1
-x = int(input('Задайте величину x\n'))
-while True:
-    accuracy = int(input('С какой точностью посчитать ряд (знаков после запятой)? (максимально 27)\n'))
-    if accuracy <= 27:  # ограничение точности в связи с работой модуля decimal
-        break
+sumOfSeries = 1  # сумма ряда, включая первый его член равный 1
 
 
-def n_term(n):
-    """Высчитывание n-го члена ряда"""
+def n_term(n, x):
+    """
+    Вычисление n-го члена ряда
+        n - номер члена ряда
+        x - значение величины x
+    """
     return Decimal(((-1) ** n) * (x ** (2 * n)) / factorial(2 * n))
 
 
-i = 0
-while True:
-    i += 1
-    _ = n_term(i)  # значение n-го члена ряда
-    if fabs(_) < 10 ** (-accuracy):  # если значение n-го члена ряда меньше требуемой точности, завершить высчитывание
-        break
-    sum_of_series = Decimal(sum_of_series + _)
-print('Сумма ряда равна:\n', sum_of_series)
+def sum_by_accuracy(x):
+    """
+    Вычисление суммы ряда с заданной точностью
+        x - значение величины x
+    """
+    global sumOfSeries
+    precision = int(input('С какой точностью посчитать ряд (знаков после запятой)? (от 1 до 28)\n'))
+
+    i = 0
+    while True:
+        i += 1
+        valueOfTerm = n_term(i, x)  # значение n-го члена ряда
+        if fabs(valueOfTerm) < 10 ** (-precision):  # е. значение n-го члена ряда меньше требуемой точности
+            break
+        sumOfSeries = Decimal(sumOfSeries + valueOfTerm)
+    return sumOfSeries
+
+
+def sum_by_amount_of_terms(x):
+    """
+    Вычисление суммы ряда с заданным количеством членов ряда
+        x - значение величины x
+    """
+    global sumOfSeries
+    amountOfTerms = int(input('Введите количество членов ряда для вычисления его суммы (>=1)\n'))  # количество членов ряда
+    for i in range(amountOfTerms - 1):  # вычитание - для первого члена ряда
+        sumOfSeries = Decimal(sumOfSeries + n_term(i, x))
+    return sumOfSeries
+
+
+if __name__ == '__main__':
+    valueOfX = int(input('Задайте величину x\n'))
+    while True:
+        _ = input('''Выберите метод подсчёта суммы ряда, введя соответствующую команду:\n[0] - по заданной точности\n[1] - с заданным количеством членов ряда\n''')
+        if _ == '0':
+            sumOfSeries = sum_by_accuracy(valueOfX)
+            break
+        elif _ == '1':
+            sumOfSeries = sum_by_amount_of_terms(valueOfX)
+            break
+    print('Сумма ряда равна:\n', sumOfSeries)
