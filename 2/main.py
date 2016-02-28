2200701f2d4fc362224cdd2650f4645169a485ef
become-iron <nikitaburkov@yandex.ru>
2016-02-28 14:44:53 +0300

    л/р №2

---
+++ 2/main.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+3. Преобразовать матрицу A(m,n) так, чтобы строки с нечетными индексами
+были упорядочены по убыванию, c четными - по возрастанию
+"""
+
+
+def convert_matrix(matrix):
+    return [(sorted(matrix[i]) if i % 2 == 0 else sorted(matrix[i], reverse=True)) for i in range(len(matrix))]
+
+
+if __name__ == '__main__':
+    from random import randrange
+    from pprint import pprint
+    m = randrange(2, 20)  # количество строк
+    n = randrange(2, 20)  # количество столбцов
+    print('Строк: {0}\nСтолбцов: {1}\n'.format(m, n))
+
+    exampleMatrix = [[randrange(10, 99) for cell in range(n)] for line in range(m)]  # генерация матрицы
+    print('Исходная матрица:')
+    pprint(exampleMatrix)
+    print('\nПреобразованная матрица:')
+    pprint(convert_matrix(exampleMatrix))
