4f0144bebae1bcb568ba67128cab02508b004742
become-iron <nikitaburkov@yandex.ru>
2016-03-01 17:54:15 +0300

    небольшие изменения

--- 2/main.py
+++ 2/main.py
@@ -12,11 +12,10 @@ def convert_matrix(matrix):
 if __name__ == '__main__':
     from random import randrange
     from pprint import pprint
-    m = randrange(2, 20)  # количество строк
-    n = randrange(2, 20)  # количество столбцов
-    print('Строк: {0}\nСтолбцов: {1}\n'.format(m, n))
-
+    m = randrange(4, 10)  # количество строк
+    n = randrange(4, 10)  # количество столбцов
     exampleMatrix = [[randrange(10, 99) for cell in range(n)] for line in range(m)]  # генерация матрицы
+    print('Строк: {0}\nСтолбцов: {1}\n'.format(m, n))
     print('Исходная матрица:')
     pprint(exampleMatrix)
     print('\nПреобразованная матрица:')
