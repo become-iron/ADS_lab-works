562adb11c456c93d8c34b23d1093646e3249dd3d
become-iron <nikitaburkov@yandex.ru>
2016-03-29 20:15:40 +0300

    Л/р 2 вар. 5. Обновление

--- 2/var5.py
+++ 2/var5.py
@@ -8,21 +8,25 @@ def is_identity_matrix(matrix):
             if (matrix[i][j] != 0 and i != j) or (matrix[i][i] != 1 and i == j):
                 is_identity = False
                 break
-        if is_identity == False: break
+        if not is_identity:
+            break
     return is_identity
 
+
 if __name__ == '__main__':
+    # генерация рандомной неединичной матрицы
     from random import randrange
     from pprint import pprint
     m = randrange(4, 10)  # количество строк
     n = randrange(4, 10)  # количество столбцов
     exampleMatrix = [[randrange(10, 99) for cell in range(n)] for line in range(m)]  # генерация матрицы
-    print('Строк: {0}\nСтолбцов: {1}\n'.format(m, n))
-    print('Исходная матрица:')
+    print('Неединичная матрица:')
     pprint(exampleMatrix)
-    print('\nПреобразованная матрица:')
     print(is_identity_matrix(exampleMatrix))
 
-    n = randrange(4, 10)
-    print(is_identity_matrix([[0 if i != j else 1 for j in range(n)] for i in range(n)]))
-
+    # генерация единичной матрицы
+    n = randrange(4, 10)  # размер матрицы
+    identity_matrix = [[0 if i != j else 1 for j in range(n)] for i in range(n)]
+    print('\nЕдиничная матрица:')
+    pprint(identity_matrix)
+    print(is_identity_matrix(identity_matrix))
