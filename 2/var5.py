cfec2f1b196b6a180c5f35fc39ab88f212e196ef
become-iron <nikitaburkov@yandex.ru>
2016-03-29 20:19:13 +0300

    Л/р 2 вар. 5. Обновление

--- 2/var5.py
+++ 2/var5.py
@@ -17,9 +17,8 @@ if __name__ == '__main__':
     # генерация рандомной неединичной матрицы
     from random import randrange
     from pprint import pprint
-    m = randrange(4, 10)  # количество строк
-    n = randrange(4, 10)  # количество столбцов
-    exampleMatrix = [[randrange(10, 99) for cell in range(n)] for line in range(m)]  # генерация матрицы
+    m = randrange(4, 10)  # размер матрицы
+    exampleMatrix = [[randrange(10, 99) for cell in range(n)] for line in range(n)]  # генерация матрицы
     print('Неединичная матрица:')
     pprint(exampleMatrix)
     print(is_identity_matrix(exampleMatrix))
