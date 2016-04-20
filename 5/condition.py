1609ac022aa362b64a6f5fd665f02ae897c5d3eb
become-iron <nikitaburkov@yandex.ru>
2016-04-20 18:35:57 +0300

    Update

---
+++ 5/condition.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+import re
+from vsptd import Triplet, TriplexString, _BID, _RE_PREFIX_NAME2
+
+# импортирование функций для реализации функции check_condition
+from math import sin, cos, tan, acos, atan, sinh, cosh, tanh, sqrt
+from math import log as ln
+from math import log10 as log
+
+
+_RE_TRIPLET = re.compile('\$([A-Za-z])\.([A-Za-z]+)=([A-Za-zА-Яа-я0-9 \"\':]*);')  # триплет
+_RE_FUNC_PRESENT = re.compile('(?:есть|ЕСТЬ)\(\$[A-Za-z]\.[A-Za-z]+\)')  # функция ЕСТЬ
+_RE_FUNC_ABSENCE = re.compile('(?:нет|НЕТ)\(\$[A-Za-z]\.[A-Za-z]+\)')  # функция НЕТ
+
+
+def strcat(a, b):
+    return a + b
+
+
+def check_condition(trpString, condition):
+    # WARN используется опасный алгоритм, который также может не всегда верно работать
+    """
+    ПРОВЕРКА ТРИПЛЕКСНОЙ СТРОКИ НА УСЛОВИЕ
+    Принимает:
+        condition (str) - условие
+    Возвращает:
+        (bool) - результат проверки условия
+    """
+    if not isinstance(condition, str) or not isinstance(condition, str):
+        raise ValueError('Должна быть строка')
+
+    # перевод трипл. строки из str в TriplexString
+    trpString = re.findall(_RE_TRIPLET, trpString)
+    tmpTrpString = []
+    for trp in trpString:
+        if trp[2] in ('True', 'False'):  # булево значение
+            value = bool(trp[2])
+        elif trp[2].startswith('\'') and trp[2].endswith('\''):  # строка
+            value = trp[2][1:-1]
+        # elif trp[2].startswith('$') and trp[2].endswith(';'):  # TODO триплет
+        #     pass
+        elif trp[2] == _BID:  # заявка
+            value = _BID
+        else:  # число
+            try:
+                value = float(trp[2]) if '.' in trp[2] else int(trp[2])
+            except ValueError:
+                raise ValueError('Неверное значение триплета')
+        tmpTrpString += [Triplet(trp[0], trp[1], value)]
+    tmpTrpString = TriplexString(*tmpTrpString)
+    trpString = tmpTrpString
+
+    # WARN возможна неверная замена
+    # например, замена слов произойдёт, даже если в условии происходит
+    # сравнение со строкой, содержащей слово на замену
+    # $W.B = ' или '
+    replacements = [[' или ', ' or '],
+                    [' и ', ' and '],
+                    [' = ', ' == '],
+                    [' <> ', ' != '],
+                    [' ^ ', ' ** ']]
+    for trp in replacements:
+        condition = condition.replace(trp[0], trp[1])
+        condition = condition.replace(trp[0].upper(), trp[1])  # для верхнего регистра
+
+    # замены для ЕСТЬ и НЕТ
+    for trp in re.findall(_RE_FUNC_PRESENT, condition):  # функция ЕСТЬ
+        item = tuple(trp[6:-1].upper().split('.'))  # извлекаем префикс и имя в кортеж
+        value = False
+        for triplet in trpString.triplets:
+            if (triplet.prefix, triplet.name) == item:
+                value = True
+                break
+        condition = condition.replace(trp, str(value))
+    for trp in re.findall(_RE_FUNC_ABSENCE, condition):  # функция НЕТ
+        item = tuple(trp[5:-1].upper().split('.'))  # извлекаем префикс и имя в кортеж
+        value = False
+        for triplet in trpString.triplets:
+            if (triplet.prefix, triplet.name) == item:
+                value = True
+                break
+        condition = condition.replace(trp, str(not value))
+
+    for trp in re.findall(_RE_PREFIX_NAME2, condition):  # замена триплетов на их значения
+        value = trpString.__getitem__(trp[1:])
+        if isinstance(value, str):  # е. значение триплета - строка, оборачиваем его в кавычки
+            value = '"{}"'.format(str(value))
+        elif isinstance(value, bool):
+            value = str(value)
+        else:
+            value = str(value)
+        condition = condition.replace(trp, value)
+
+    # print('Конечное выражение: ', condition, sep='')
+    return eval(condition)
--- 5/vsptd.py
+++ 5/vsptd.py
@@ -1,34 +1,15 @@
 # -*- coding: utf-8 -*-
 
 import re
-from math import sin, cos, tan, acos, atan, sinh, cosh, tanh, sqrt
-from math import log as ln
-from math import log10 as log
+
 
 _BID = ':'  # заявка (запрос полной информации по заданному объекту, определяемому префиксом)
 
 _RE_PREFIX = re.compile('^[A-Za-z]$')  # префикс: 1 латинский символ
 _RE_NAME = re.compile('^[A-Za-z]+$')  # имя: латинские символы
-_RE_VALUE = re.compile('^[A-Za-zА-Яа-я0-9]*$')  # значение
+_RE_VALUE = re.compile('^[A-Za-zА-Яа-я0-9 ]*$')  # значение
 _RE_PREFIX_NAME = re.compile('^[A-Za-z]\.[A-Za-z]+$')  # префикс.имя
 _RE_PREFIX_NAME2 = re.compile('\$[A-Za-z]\.[A-Za-z]+')  # $префикс.имя
-_RE_FUNC_PRESENT = re.compile('(?:есть|ЕСТЬ)\(\$[A-Za-z]\.[A-Za-z]+\)')  # функция ЕСТЬ
-_RE_FUNC_ABSENCE = re.compile('(?:нет|НЕТ)\(\$[A-Za-z]\.[A-Za-z]+\)')  # функция НЕТ
-
-
-# def parse_to_triplets(string='', file=None, count=-1):
-#     """Парсинг строки/файла в триплеты
-#     """
-#     if file is not None:
-#         pass
-#     elif isinstance(string, str):
-#         pass
-#     else:
-#         raise ValueError
-
-
-def strcat(a, b):
-    return a + b
 
 
 class Triplet:
@@ -95,38 +76,38 @@ class TriplexString:
         for _ in triplets:  # CHECK проверить скорость работы через filter
             if not isinstance(_, Triplet):
                 raise ValueError('Аргументы должны быть триплетами')
-        self.trpString = list(triplets)
+        self.triplets = list(triplets)
 
         # удаление повторов триплетов (по префиксам и именам)
-        trpString_copy = self.trpString.copy()
+        trpString_copy = self.triplets.copy()
         for triplet in trpString_copy:
             # триплеты с данными префиксами и именами
-            triplets_to_remove = [_ for _ in self.trpString if _.prefix == triplet.prefix and _.name == triplet.name]
+            triplets_to_remove = [_ for _ in self.triplets if _.prefix == triplet.prefix and _.name == triplet.name]
             triplets_to_remove = triplets_to_remove[:-1]  # исключение последнего найденного триплета
             for secTriplet in triplets_to_remove:
-                self.trpString.remove(secTriplet)
+                self.triplets.remove(secTriplet)
 
     def __len__(self):
-        return len(self.trpString)
+        return len(self.triplets)
 
     def __add__(self, other):
         # CHECK
         if isinstance(other, Triplet):
-            return TriplexString(*(self.trpString + [other]))
+            return TriplexString(*(self.triplets + [other]))
         elif isinstance(other, TriplexString):
-            return TriplexString(*(self.trpString + other.trpString))
+            return TriplexString(*(self.triplets + other.triplets))
         else:
             raise ValueError
 
     def __str__(self):
-        return ''.join(tuple(str(triplet) for triplet in self.trpString))
+        return ''.join(tuple(str(triplet) for triplet in self.triplets))
 
     def __contains__(self, item):
         # TODO возможно, стоит включить возможность проверки включения по префиксу и имени
         if not isinstance(item, Triplet):
             raise ValueError('Должен быть триплет')
 
-        for triplet in self.trpString:
+        for triplet in self.triplets:
             if triplet.prefix == item.prefix and triplet.name == item.name and triplet.value == item.value:
                 return True
         return False
@@ -135,32 +116,32 @@ class TriplexString:
         # TODO CHECK
         if isinstance(key, str):  # элемент по ключу
             if re.match(_RE_PREFIX, key) is not None:  # получить триплеты по префиксу в виде триплесной строки
-                return TriplexString(*[triplet for triplet in self.trpString if triplet.prefix == key])
+                return TriplexString(*[triplet for triplet in self.triplets if triplet.prefix == key])
             elif re.match(_RE_PREFIX_NAME, key) is not None:  # получить значение по префиксу и имени
                 key = key.upper().split('.')
-                for triplet in self.trpString:
+                for triplet in self.triplets:
                     if triplet.prefix == key[0] and triplet.name == key[1]:
                         return triplet.value
                 return None
             else:
                 raise ValueError('Неверный формат данных')
         else:  # элемент по срезу
-            return self.trpString[key]
+            return self.triplets[key]
 
     def __eq__(self, other):
         # CHECK возможно, стоит замерить скорость работы
         if not isinstance(other, TriplexString):
             raise ValueError
 
-        if len(self.trpString) != len(other):
+        if len(self.triplets) != len(other):
             return False
         for triplet in other:
-            if triplet not in self.trpString:
+            if triplet not in self.triplets:
                 return False
         return True
 
     def __iter__(self):
-        return iter(self.trpString)
+        return iter(self.triplets)
 
     def add(self, other):
         """
@@ -191,9 +172,9 @@ class TriplexString:
         if re.match(_RE_NAME, name) is None:
             raise ValueError('Неверный формат имени')
 
-        for triplet in self.trpString:
+        for triplet in self.triplets:
             if triplet.prefix == prefix and triplet.name == name:
-                self.trpString.remove(triplet)
+                self.triplets.remove(triplet)
                 return
         raise ValueError('Триплет не найден')
 
@@ -207,64 +188,6 @@ class TriplexString:
         if not isinstance(prefix, str):
             raise ValueError('Должен быть триплет')
 
-        for triplet in self.trpString:
+        for triplet in self.triplets:
             if triplet.prefix == prefix:
-                self.trpString.remove(triplet)
-
-    def check_condition(self, condition):
-        # WARN используется опасный алгоритм, который также может не всегда верно работать
-        """
-        ПРОВЕРКА ТРИПЛЕКСНОЙ СТРОКИ НА УСЛОВИЕ
-        Принимает:
-            condition (str) - условие
-        Возвращает:
-            (bool) - результат проверки условия
-        """
-        if not isinstance(condition, str):
-            raise ValueError('Должна быть строка')
-
-        # WARN возможна неверная замена
-        # например, замена слов произойдёт, даже если в условии происходит
-        # сравнение со строкой, содержащей слово на замену
-        # $W.B = " или "
-        replacements = [[' или ', ' or '],
-                        [' и ', ' and '],
-                        [' = ', ' == '],
-                        [' <> ', ' != '],
-                        [' ^ ', ' ** ']]
-        for _ in replacements:
-            condition = condition.replace(_[0], _[1])
-            condition = condition.replace(_[0].upper(), _[1])  # для верхнего регистра
-
-        # замены для ЕСТЬ и НЕТ
-        # TODO оптимизировать
-        for _ in re.findall(_RE_FUNC_PRESENT, condition):  # функция ЕСТЬ
-            item = _[6:-1].upper().split('.')
-            val = False
-            for triplet in self.trpString:
-                if triplet.prefix == item[0] and triplet.name == item[1]:
-                    val = True
-                    break
-            condition = condition.replace(_, str(val))
-        for _ in re.findall(_RE_FUNC_ABSENCE, condition):  # функция НЕТ
-            item = _[5:-1].upper().split('.')
-            val = False
-            for triplet in self.trpString:
-                if triplet.prefix == item[0] and triplet.name == item[1]:
-                    val = True
-                    break
-            condition = condition.replace(_,
-                                          'False' if val is True else 'True')
-
-        for _ in re.findall(_RE_PREFIX_NAME2, condition):  # замена триплетов на их значения
-            val = self.__getitem__(_[1:])
-            if isinstance(val, str):  # е. значение триплета - строка, оборачиваем его в кавычки
-                val = '"{}"'.format(str(val))
-            elif isinstance(val, bool):
-                val = str(val)
-            else:
-                val = str(val)
-            condition = condition.replace(_, val)
-
-        # print('Конечное выражение:\n', condition, '\n', sep='')
-        return eval(condition)
+                self.triplets.remove(triplet)
