a56e89c800c608acdddf71c01e41c42fb9ced45f
become-iron <nikitaburkov@yandex.ru>
2016-05-04 23:43:24 +0300

    Мелкие правки

--- 5/work_example/test.py
+++ 5/work_example/test.py
@@ -7,13 +7,13 @@ sys.path.append('..\\')
 from condition import check_condition
 
 FILE_STRING = 'triplex_string.txt'  # имя файла с трипл. строкой
-FILE_STRING_FROM_DB = 'triplex_string_from_DB.txt'
+FILE_STRING_FROM_DB = 'triplex_string_from_DB.txt'  # имя файла с трипл. строкой по данным из базы
 FILE_COND = 'conditions.txt'  # имя файла с условиями
 
 trpString = open(FILE_STRING, 'r', encoding='utf-8').read()  # трипл. строка
 trpStringFromDB = open(FILE_STRING_FROM_DB, 'r', encoding='utf-8').read()  # трипл. строка из базы данных
 conditions = open(FILE_COND, 'r', encoding='utf-8').read().split('\n')  # условия
-results = [check_condition(trpString, condition, trpStringFromDB) for condition in conditions]  # результаты проверки условий
+results = [check_condition(trpString, condition, trpStringFromDB) for condition in conditions]  # результаты проверки
 
 print('Исходная триплексная строка:', trpString, sep='\n', end='\n\n')
 if len(FILE_STRING_FROM_DB) > 0:
